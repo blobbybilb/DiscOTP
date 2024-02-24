5d1b679fb015327a77546f760a8eddcd40f004dc
blobbybilb <notablob@outlook.com>
2024-02-23 18:47:15 -0800

    save

--- data.py
+++ data.py
@@ -1,44 +1,99 @@
-import requests
-import json
-
-WHY_DB_GET_URL = 'http://127.0.0.1:5000/get/totpbot/'
-WHY_DB_SET_URL = 'http://127.0.0.1:5000/set/totpbot/'
-
-ALLOWED_ROLE_KEY = '--allowed_role--'
-
-datatype = dict[str, str]
+from sqlitedict import SqliteDict  # type: ignore
+from typing import TypedDict
 
 
-def get(guild_id: int) -> datatype:
-    return json.loads(requests.get(WHY_DB_GET_URL + str(guild_id)).text)
+class Account(TypedDict):
+    name: str
+    secret: str
 
 
-def set(guild_id: int, value: datatype) -> str:
-    # return requests.get(WHY_DB_SET_URL + str(guild_id) + '/' + json.dumps(value)).text
-    return requests.post(WHY_DB_SET_URL + str(guild_id), json=value).text
+class ServerConfig(TypedDict):
+    id: int
+    auth_role: int
+    auth_channel: int
+    accounts: list[Account]
 
 
-def add_account(guild_id: int, account: str, secret: str) -> str:
-    data = get(guild_id)
-    data[account] = secret
-    return set(guild_id, data)
+db = SqliteDict("data.sqlite", autocommit=True)
 
 
-def remove_account(guild_id: int, account: str) -> str:
-    data = get(guild_id)
-    del data[account]
-    return set(guild_id, data)
+def get_server_config(server_id: int) -> ServerConfig | None:
+    try:
+        return db[str(server_id)]
+    except KeyError:
+        return None
 
 
-def set_allowed_role(guild_id: int, role_id: int) -> str:
-    data = get(guild_id)
-    data[ALLOWED_ROLE_KEY] = role_id
-    return set(guild_id, data)
+def set_server_config(server_id: int, config: ServerConfig):
+    db[str(server_id)] = config
 
 
-def get_allowed_role(guild_id: int) -> int:
-    return get(guild_id)[ALLOWED_ROLE_KEY]
+def is_configured(server_id: int) -> bool:
+    return get_server_config(server_id) is not None
 
 
-def create_guild(guild_id: int) -> str:
-    return set(guild_id, {})
+def get_account(server_id: int, account_name: str) -> Account | None:
+    config = get_server_config(server_id)
+    if config is None:
+        return None
+
+    for account in config["accounts"]:
+        if account["name"] == account_name:
+            return account
+
+    return None
+
+
+def account_exists(server_id: int, account_name: str) -> bool:
+    return get_account(server_id, account_name) is not None
+
+
+def add_account(server_id: int, account: Account) -> bool:
+    config = get_server_config(server_id)
+    if config is None:
+        print(2, server_id, account)
+        return False
+
+    if account_exists(server_id, account["name"]):
+        print(3)
+        return False
+
+    config["accounts"].append(account)
+    set_server_config(server_id, config)
+    return True
+
+
+def delete_account(server_id: int, account_name: str) -> bool:
+    config = get_server_config(server_id)
+    if config is None:
+        return False
+
+    for i, account in enumerate(config["accounts"]):
+        if account["name"] == account_name:
+            del config["accounts"][i]
+            set_server_config(server_id, config)
+            return True
+
+    return False
+
+
+def update_account(server_id: int, account: Account) -> bool:
+    config = get_server_config(server_id)
+    if config is None:
+        return False
+
+    for i, a in enumerate(config["accounts"]):
+        if a["name"] == account["name"]:
+            config["accounts"][i] = account
+            set_server_config(server_id, config)
+            return True
+
+    return False
+
+
+def get_account_names(server_id: int) -> list[str]:
+    config = get_server_config(server_id)
+    if config is None:
+        return []
+
+    return [account["name"] for account in config["accounts"]]
--- old/bot.py
+++ old/bot.py
@@ -4,26 +4,27 @@ from discord.ext.commands import Context as Ctx
 import pyotp
 import data
 
-'''
+"""
 TODO:
 - error handling
-'''
+"""
 
 
 bot = discord.Bot()
 
 
 def user_has_permission(ctx: Ctx):
-    return (ctx.author.guild_permissions.administrator
-            or ctx.guild.get_role(data.get_allowed_role(ctx.guild.id)) in ctx.author.roles
-            )
+    return (
+        ctx.author.guild_permissions.administrator
+        or ctx.guild.get_role(data.get_allowed_role(ctx.guild.id)) in ctx.author.roles
+    )
 
 
 @bot.slash_command()
 async def code(ctx: Ctx, account: str):
-    '''Generates a TOTP code for the given account'''
+    """Generates a TOTP code for the given account"""
     if not user_has_permission(ctx):
-        return await ctx.respond('You do not have permission to use this command.')
+        return await ctx.respond("You do not have permission to use this command.")
     try:
         totp = pyotp.TOTP(data.get(ctx.guild.id)[account])
     except KeyError:
@@ -31,30 +32,32 @@ async def code(ctx: Ctx, account: str):
 
     theCode = totp.now()
 
-    return await ctx.respond(f"{account} code: __**{theCode}**__\n\nExpires in: {int(30 - time() % 30)} seconds")
+    return await ctx.respond(
+        f"{account} code: __**{theCode}**__\n\nExpires in: {int(30 - time() % 30)} seconds"
+    )
 
 
 @bot.slash_command()
 async def add(ctx: Ctx, account: str, secret: str):
-    '''Adds an account'''
+    """Adds an account"""
     if not user_has_permission(ctx):
-        return await ctx.respond('You do not have permission to use this command.')
+        return await ctx.respond("You do not have permission to use this command.")
     data.add_account(ctx.guild.id, account, secret)
     return await ctx.respond(f"Added account `{account}`")
 
 
 @bot.slash_command()
 async def remove(ctx: Ctx, account: str):
-    '''Remove an account'''
+    """Remove an account"""
     if not user_has_permission(ctx):
-        return await ctx.respond('You do not have permission to use this command.')
-    data.remove_account(ctx.guild.id, account)
+        return await ctx.respond("You do not have permission to use this command.")
+    data.delete_account(ctx.guild.id, account)
     return await ctx.respond(f"Account `{account}` removed")
 
 
 @bot.slash_command()
 async def set_allowed_role(ctx: Ctx, role: discord.Role):
-    '''Allows users with the specified role to use the bot'''
+    """Allows users with the specified role to use the bot"""
     if not user_has_permission(ctx):
         return await ctx.respond("You are not an administrator")
 
@@ -64,28 +67,32 @@ async def set_allowed_role(ctx: Ctx, role: discord.Role):
 
 @bot.slash_command()
 async def create(ctx: Ctx):
-    '''Creates/Resets server's stored accounts'''
+    """Creates/Resets server's stored accounts"""
     if not user_has_permission(ctx):
-        return await ctx.respond('You do not have permission to use this command.')
+        return await ctx.respond("You do not have permission to use this command.")
     data.create_guild(ctx.guild.id)
     await ctx.respond("Created/reset storage file for this guild")
 
 
 @bot.slash_command()
 async def list(ctx: Ctx):
-    '''Lists all accounts'''
+    """Lists all accounts"""
     if not user_has_permission(ctx):
-        return await ctx.respond('You do not have permission to use this command.')
+        return await ctx.respond("You do not have permission to use this command.")
     accounts = data.get(ctx.guild.id)
-    await ctx.respond(', '.join(accounts.keys()).replace(', --allowed_role--', '').replace('--allowed_role--', ''))
+    await ctx.respond(
+        ", ".join(accounts.keys())
+        .replace(", --allowed_role--", "")
+        .replace("--allowed_role--", "")
+    )
 
 
 # help command
-@ bot.slash_command()
+@bot.slash_command()
 async def help(ctx: Ctx):
-    '''Shows help'''
+    """Shows help"""
     await ctx.respond(
-        '''
+        """
 ** Commands: **
 
 `/ code <account>`: Get the code for an account
@@ -95,7 +102,8 @@ async def help(ctx: Ctx):
 `/ set_allowed_role <role>`: Set the role that can use the bot(TODO)
 
 Note: This bot is open source and can be self-hosted. More info at <https://github.com/blobbybilb/TOTP-discord-bot>.
-'''
+"""
     )
+
 
 bot.run("token")
